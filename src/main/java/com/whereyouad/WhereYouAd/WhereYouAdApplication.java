8d2ca4e0e1cbb3b64fa890f5b40aa01d765532b3
jinnieusLab <kitty14904@naver.com>
2026-01-14 02:29:48 +0900

    :sparkles: feat: project setting

---
+++ src/main/java/com/whereyouad/WhereYouAd/WhereYouAdApplication.java
@@ -0,0 +1,13 @@
+package com.whereyouad.WhereYouAd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereYouAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereYouAdApplication.class, args);
+	}
+
+}
