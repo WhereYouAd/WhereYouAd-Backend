bab75a458386d6e96e36325ad323a7a0fb2c46ed
오준영 <ojy654@naver.com>
2026-01-15 21:18:04 +0900

    :sparkles: feat: BaseEntity 추가 및 적용

--- src/main/java/com/whereyouad/WhereYouAd/WhereYouAdApplication.java
+++ src/main/java/com/whereyouad/WhereYouAd/WhereYouAdApplication.java
@@ -2,8 +2,10 @@ package com.whereyouad.WhereYouAd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WhereYouAdApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/whereyouad/WhereYouAd/global/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.whereyouad.WhereYouAd.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity { //BaseEntity 활용 생성 시간, 변경 시간 반영
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
