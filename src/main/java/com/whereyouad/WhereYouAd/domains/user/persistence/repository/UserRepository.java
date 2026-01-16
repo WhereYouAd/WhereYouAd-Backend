5b2d39941d93cde40f6f6bc8da77a79aa080a5a9
오준영 <ojy654@naver.com>
2026-01-16 21:09:17 +0900

    :sparkles: feat: 회원가입 시 이메일 중복 방지를 위한 UserRepository 이메일 조회 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/UserRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/UserRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.email = :email")
     Optional<User> findUserByEmail(@Param("email") String email);
+
+    //이메일 중복 예외 처리를 위한 이메일로 조회 메서드
+    boolean existsByEmail(String email);
 }
