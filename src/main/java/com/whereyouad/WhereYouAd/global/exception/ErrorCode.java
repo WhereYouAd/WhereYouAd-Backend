5b766838d94aa220d19d7f2b693f924b5ccc6092
김민규 <alsrb0332@naver.com>
2026-01-17 16:30:15 +0900

    :sparkles: feat: 응답 구조 추가

---
+++ src/main/java/com/whereyouad/WhereYouAd/global/exception/ErrorCode.java
@@ -0,0 +1,25 @@
+package com.whereyouad.WhereYouAd.global.exception;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements BaseErrorCode{
+
+    //400
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "COMMON-001"),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다.", "COMMON-002"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다.", "COMMON-003"),
+
+    //500
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.", "COMMON-004"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final String code;
+}
