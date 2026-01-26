4de758a0007b84f3f81b85e5b8b2ab5b58b59257
오준영 <ojy654@naver.com>
2026-01-26 22:20:37 +0900

    :sparkles: feat: 이메일 인증관련 Request, Response DTO 추가

---
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/application/dto/request/EmailVerifyRequest.java
@@ -0,0 +1,13 @@
+package com.whereyouad.WhereYouAd.domains.user.application.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailVerifyRequest(
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email,
+        @NotBlank
+        String authCode
+) {
+}
