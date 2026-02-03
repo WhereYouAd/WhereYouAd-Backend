a8828f86a543b18d850effb03e37a92c1200482b
오준영 <ojy654@naver.com>
2026-02-03 16:09:21 +0900

    :sparkles: feat: 조직 Request DTO 검증 어노테이션 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/request/OrgRequest.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/request/OrgRequest.java
@@ -1,8 +1,11 @@
 package com.whereyouad.WhereYouAd.domains.organization.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class OrgRequest {
 
     public record Create (
+            @NotBlank(message = "조직 이름은 필수입니다.")
             String name,
             String description,
             String logoUrl
