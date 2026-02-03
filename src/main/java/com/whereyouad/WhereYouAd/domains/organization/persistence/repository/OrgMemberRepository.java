c0ce3cc14ee9b1448992e8e4da3f738da1d71696
오준영 <ojy654@naver.com>
2026-02-03 15:29:14 +0900

    :sparkles: feat: OrgMemberRepository 내부 User 를 통해 OrgMember 조회 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
@@ -1,7 +1,16 @@
 package com.whereyouad.WhereYouAd.domains.organization.persistence.repository;
 
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.OrgMember;
+import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
+import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrgMemberRepository extends JpaRepository<OrgMember, Long> {
+
+    List<OrgMember> findOrgMemberByUser(User user);
 }
