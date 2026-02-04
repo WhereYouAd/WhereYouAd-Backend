776af7c0e2de79f18e100548e7b60a8b57750d69
오준영 <ojy654@naver.com>
2026-02-04 21:30:05 +0900

    Revert ":sparkles: feat: 조직 수정 관련 오류코드 추가"
    This reverts commit 1f364dd34d954d1ea53f0b410c3d02c5e7a01bdc.

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/exception/code/OrgErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/exception/code/OrgErrorCode.java
@@ -9,14 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum OrgErrorCode implements BaseErrorCode {
     //400
-    ORG_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "ORG_400_1", "사용자가 이미 속해있는 조직의 이름입니다."),
-
-    //403
-    ORG_UPDATE_FORBIDDEN(HttpStatus.FORBIDDEN, "ORG_403_1", "조직 정보 변경은 해당 조직을 생성한 회원만 가능합니다."),
-
-    //404
-    ORG_NOT_FOUND(HttpStatus.NOT_FOUND, "ORG_404_1", "해당 id 의 조직이 존재하지 않습니다."),
-    ;
+    ORG_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "ORG_400_1", "사용자가 이미 속해있는 조직의 이름입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
