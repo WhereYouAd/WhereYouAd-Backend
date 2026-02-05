873a780e76c85e089f4d6420944412a70c8f9319
오준영 <ojy654@naver.com>
2026-02-05 15:52:11 +0900

    :sparkles: feat: Hard Delete 를 위한 OrgMemberRepository 내부 Organization 으로 OrgMember 조회 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
@@ -1,8 +1,11 @@
 package com.whereyouad.WhereYouAd.domains.organization.persistence.repository;
 
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.OrgMember;
+import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
 import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,8 @@ public interface OrgMemberRepository extends JpaRepository<OrgMember, Long> {
 
     //User 가 가진 OrgMember 모두 추출하는 메서드
     List<OrgMember> findOrgMemberByUser(User user);
+
+    //특정 Organization 에 속한 OrgMember 모두 추출하는 메서드
+    @Query("select om from OrgMember om where om.organization = :organization")
+    List<OrgMember> findOrgMemberByOrg(@Param(value = "organization") Organization organization);
 }
