58c482f716d5e68115aae9c5b106130bc074cee0
오준영 <ojy654@naver.com>
2026-02-17 20:53:06 +0900

    :sparkles: feat: 조직 조회를 위한 Response DTO, Converter 코드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/response/OrgResponse.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/response/OrgResponse.java
@@ -1,6 +1,9 @@
 package com.whereyouad.WhereYouAd.domains.organization.application.dto.response;
 
+import com.whereyouad.WhereYouAd.domains.organization.domain.constant.OrgRole;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class OrgResponse {
 
@@ -9,8 +12,59 @@ public class OrgResponse {
             LocalDateTime createdAt
     ) {}
 
-    public record Read (
-        //TODO
+    //내 조직 정보 조회를 위한 간략화된 조직 정보 DTO
+    public record SimpleInfo(
+            Long orgId,
+            String name,
+            String description,
+            String logoUrl,
+            OrgRole myRole
+    ) {}
+
+    //내 조직 정보는 SimpleInfo 를 List 로 반환하고, 페이징 메타데이터를 함께 반환
+    //MyOrganization DTO 내부에 SimpleInfo DTO 가 여러개 포함
+    public record MyOrganizations(
+            List<SimpleInfo> organizations,
+            long totalElements,
+            int totalPages,
+            boolean hasNext
+    ) {}
+
+    //하나의 조직 세부 정보 반환 DTO
+    //OrgDetail DTO 내부에 OrgMembers DTO 가 여러개 포함
+    public record OrgDetail (
+            Long orgId,
+            String name,
+            String description,
+            String logoUrl,
+            LocalDateTime createdAt,
+            List<OrgMembers> members
+    ) { }
+
+    //조직 세부 정보 반환을 위한 간략화된 회원 정보 반환 DTO
+    public record OrgMembers(
+            Long userId,
+            String name,
+            String email,
+            OrgRole role
+    ) {}
+
+    //이름으로 조회시 반환 DTO
+    //OrgSearchList DTO 내부에 ListInfo DTO 가 여러개 포함
+    public record OrgSearchList(
+            String query,
+            List<ListInfo> organizations,
+            long totalElements,
+            int totalPages,
+            boolean hasNext
+    ) {}
+
+    //조직 이름으로 조회시 반환을 위한 각 조직의 간략화된 정보 DTO
+    public record ListInfo(
+            Long orgId,
+            String name,
+            String description,
+            String logoUrl
     ) {}
 
     public record Update (
@@ -26,5 +80,6 @@ public class OrgResponse {
             Long orgId,
             String message
     ) {}
+
 
 }
--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/mapper/OrgConverter.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/mapper/OrgConverter.java
@@ -2,8 +2,14 @@ package com.whereyouad.WhereYouAd.domains.organization.application.mapper;
 
 import com.whereyouad.WhereYouAd.domains.organization.application.dto.request.OrgRequest;
 import com.whereyouad.WhereYouAd.domains.organization.application.dto.response.OrgResponse;
+import com.whereyouad.WhereYouAd.domains.organization.domain.constant.OrgRole;
 import com.whereyouad.WhereYouAd.domains.organization.domain.constant.OrgStatus;
+import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.OrgMember;
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
+import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public class OrgConverter {
 
@@ -23,6 +29,70 @@ public class OrgConverter {
     public static OrgResponse.Delete toRestoredResponse(Organization organization) {
         return new OrgResponse.Delete(organization.getId(),
                 "해당 조직이 활성화 되었습니다");
+    }
+
+    public static OrgResponse.ListInfo toListInfo(Organization organization) {
+        return new OrgResponse.ListInfo(organization.getId(),
+                organization.getName(),
+                organization.getDescription(),
+                organization.getLogoUrl()
+        );
+    }
+
+    public static OrgResponse.MyOrganizations toMyOrganizations(Page<OrgResponse.SimpleInfo> page) {
+        return new OrgResponse.MyOrganizations(
+                page.getContent(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.hasNext()
+        );
+    }
+
+    public static OrgResponse.OrgSearchList toOrgSearchList(String query, Page<OrgResponse.ListInfo> page) {
+        return new OrgResponse.OrgSearchList(
+                query,
+                page.getContent(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.hasNext()
+        );
+    }
+
+    public static OrgResponse.OrgMembers toOrgMembers(User user, Organization organization) {
+        OrgRole role;
+
+        if (organization.getOwnerUserId().equals(user.getId())) {
+            role = OrgRole.ADMIN;
+        } else {
+            role = OrgRole.MEMBER;
+        }
+        return new OrgResponse.OrgMembers(user.getId(), user.getName(), user.getEmail(), role);
+    }
+
+    public static OrgResponse.OrgDetail toOrgDetail(Organization organization, List<OrgResponse.OrgMembers> orgMembers) {
+        return new OrgResponse.OrgDetail(organization.getId(),
+                organization.getName(),
+                organization.getDescription(),
+                organization.getLogoUrl(),
+                organization.getCreatedAt(),
+                orgMembers
+        );
+    }
+
+    public static OrgResponse.SimpleInfo toOrgSimpleInfo(Organization organization, Long userId) {
+        OrgRole myRole;
+
+        if (organization.getOwnerUserId().equals(userId)) {
+            myRole = OrgRole.ADMIN;
+        } else {
+            myRole = OrgRole.MEMBER;
+        }
+
+        return new OrgResponse.SimpleInfo(organization.getId(),
+                organization.getName(),
+                organization.getDescription(),
+                organization.getLogoUrl(),
+                myRole);
     }
 
     //DTO -> Entity
