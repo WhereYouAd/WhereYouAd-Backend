67fc0c1b47b3df57d936808d1a02b1ae5cac2e70
오준영 <ojy654@naver.com>
2026-02-17 20:53:42 +0900

    :sparkles: feat: 조직 조회를 위한 OrgRepository, OrgMemberRepository 커스텀 조회 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgRepository.java
@@ -1,7 +1,15 @@
 package com.whereyouad.WhereYouAd.domains.organization.persistence.repository;
 
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrgRepository extends JpaRepository<Organization, Long> {
+
+    //검색하려는 문자열이 포함된 name 을 가진 조직을 추출
+    @Query("select o from Organization o where o.name like concat('%', :name, '%') AND o.status = 'ACTIVE'")
+    Page<Organization> findOrganizationsByName(@Param(value = "name") String name, Pageable pageable);
 }
