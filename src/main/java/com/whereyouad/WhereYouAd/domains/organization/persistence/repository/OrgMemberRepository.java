f28a53047dab8fca90cd5325bdd88acaf82d68b8
오준영 <ojy654@naver.com>
2026-02-18 13:57:01 +0900

    :bug: fix: OrgMemberRepository 내부 페이징 관련 메서드 -> 단순 List 반환으로 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/repository/OrgMemberRepository.java
@@ -3,8 +3,6 @@ package com.whereyouad.WhereYouAd.domains.organization.persistence.repository;
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.OrgMember;
 import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
 import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,10 +14,9 @@ public interface OrgMemberRepository extends JpaRepository<OrgMember, Long> {
     //User 가 가진 OrgMember 모두 추출하는 메서드
     List<OrgMember> findOrgMemberByUser(User user);
 
-    //userId 를 통해 OrgMember 추출
-    @Query(value = "select om from OrgMember om join fetch om.organization o where om.user.id = :userId and o.status = 'ACTIVE'",
-            countQuery = "select count(om) from OrgMember om join om.organization o where om.user.id = :userId and o.status = 'ACTIVE'")
-    Page<OrgMember> findOrgMemberByUserId(@Param("userId") Long userId, Pageable pageable);
+    //userId 를 통해 OrgMember 추출 -> Organization 의 status 가 ACTIVE 인 경우에만 조회
+    @Query(value = "select om from OrgMember om join fetch om.organization o where om.user.id = :userId and o.status = 'ACTIVE'")
+    List<OrgMember> findOrgMemberByUserId(@Param("userId") Long userId);
 
     //특정 Organization 에 속한 OrgMember 모두 추출하는 메서드
     @Query("select om from OrgMember om where om.organization = :organization")
