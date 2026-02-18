d8e9c40794fc8634aa3d74a2134c3388a4f5ef45
오준영 <ojy654@naver.com>
2026-02-18 14:09:53 +0900

    :art: style: 주석 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/response/OrgResponse.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/application/dto/response/OrgResponse.java
@@ -21,14 +21,13 @@ public class OrgResponse {
             OrgRole myRole
     ) {}
 
-    //내 조직 정보는 SimpleInfo 를 List 로 반환하고, 페이징 메타데이터를 함께 반환
+    //내 조직 정보는 SimpleInfo 를 List 로 반환
     //MyOrganization DTO 내부에 SimpleInfo DTO 가 여러개 포함
     public record MyOrganizations(
             List<SimpleInfo> organizations
     ) {}
 
     //하나의 조직 세부 정보 반환 DTO
-    //OrgDetail DTO 내부에 OrgMembers DTO 가 여러개 포함
     public record OrgDetail (
             Long orgId,
             String name,
