a32114f3f78e7fab5277474ba7a7a22be789bd05
ojy0903 <ojy654@naver.com>
2026-02-21 21:41:29 +0900

    :sparkles: feat: 소셜로그인 구분을 위한 기존 EmailSentResponse 수정 및 PasswordResetResponse 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/user/application/dto/response/EmailSentResponse.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/application/dto/response/EmailSentResponse.java
@@ -1,8 +1,12 @@
 package com.whereyouad.WhereYouAd.domains.user.application.dto.response;
 
+import com.whereyouad.WhereYouAd.domains.user.domain.constant.Provider;
+
 public record EmailSentResponse(
         String message, //인증코드를 이메일로 전송했습니다.
         String email, //전송한 이메일
-        long expireIn //만료시간 (500L -> 500초)
+        Long expireIn, //만료시간 (500L -> 500초)
+        boolean isProviderLinked,
+        Provider providerType
 ) {
 }
