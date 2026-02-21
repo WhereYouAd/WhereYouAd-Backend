7c22bc05a0a3eedb7efe08de215544a97d5a9bfb
ojy0903 <ojy654@naver.com>
2026-02-21 21:41:49 +0900

    :sparkles: feat: 소셜로그인 구분을 위한 AuthProviderAccountRepository 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/AuthProviderAccountRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/AuthProviderAccountRepository.java
@@ -1,8 +1,16 @@
 package com.whereyouad.WhereYouAd.domains.user.persistence.repository;
 
 import com.whereyouad.WhereYouAd.domains.user.persistence.entity.AuthProviderAccount;
+import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface AuthProviderAccountRepository extends JpaRepository<AuthProviderAccount, Long> {
     AuthProviderAccount findByProviderId(String username);
+
+    @Query("select apa from AuthProviderAccount apa where apa.user = :user")
+    Optional<AuthProviderAccount> findByUser(@Param(value = "user") User user);
 }
