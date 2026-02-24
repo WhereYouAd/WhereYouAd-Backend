15cbf5e330bb8ced522f28f70f4b96de5069de6e
김민규 <alsrb0332@naver.com>
2026-02-24 21:18:53 +0900

    ✨ feat: 플랫폼 별 응답 구조 생성

---
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/kakao/converter/KakaoConverter.java
@@ -0,0 +1,5 @@
+package com.whereyouad.WhereYouAd.infrastructure.client.kakao.converter;
+
+public class KakaoConverter {
+    // KakaoDTO -> MetricFactResponse 변환
+}
---
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/naver/converter/NaverConverter.java
@@ -0,0 +1,5 @@
+package com.whereyouad.WhereYouAd.infrastructure.client.naver.converter;
+
+public class NaverConverter {
+    // NaverDTO -> MetricFactResponse 변환
+}
