4627b38a02b4e785d1e47a7ea7c886b0eefeb783
오준영 <ojy654@naver.com>
2026-03-02 17:34:48 +0900

    :sparkles: feat: ProjectErrorCode 내부 조직에 속하지 않은 회원의 요청에 대한 예외코드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/project/exception/code/ProjectErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/project/exception/code/ProjectErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ProjectErrorCode implements BaseErrorCode {
 
+    //403
+    ACCESS_FORBIDDEN(HttpStatus.FORBIDDEN, "AD_403_1", "해당 프로젝트에 대한 접근 권한이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
