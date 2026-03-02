0cd799d9308a90f6e4a141a0dd294dbb17b772d7
김민규 <alsrb0332@naver.com>
2026-03-02 21:37:26 +0900

    ✨ feat: 진행 중인 광고 조회 컨트롤러 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/presentation/DashboardController.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/presentation/DashboardController.java
@@ -6,12 +6,12 @@ import com.whereyouad.WhereYouAd.domains.dashboard.presentation.docs.DashboardCo
 import com.whereyouad.WhereYouAd.global.response.DataResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,4 +30,20 @@ public class DashboardController implements DashboardControllerDocs {
         return ResponseEntity.ok(DataResponse.from(budgetSummaryResponse));
     }
 
+    @GetMapping("/{orgId}/ad-count")
+    public ResponseEntity<DataResponse<DashboardResponse.OngoingPlatformAdCountResponse>> getOngoingAdCount(
+            @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable Long orgId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate)
+    {
+        // 조회 기간 설정이 없으면 오늘부터 최근 1개월 데이터 조회
+        LocalDate adjustedEnd = (endDate != null) ? endDate : LocalDate.now();
+        LocalDate adjustedStart = (startDate != null) ? startDate : adjustedEnd.minusMonths(1);
+
+        DashboardResponse.OngoingPlatformAdCountResponse response = dashboardService.getOngoingAdCountByProvider(
+                userId, orgId, adjustedStart, adjustedEnd);
+
+        return ResponseEntity.ok(DataResponse.from(response));
+    }
 }
