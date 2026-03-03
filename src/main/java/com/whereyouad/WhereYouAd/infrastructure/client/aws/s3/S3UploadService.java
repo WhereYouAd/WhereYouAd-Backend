9992abaf64d0ddf573e4bc3a79d48552eeb4401e
jinnieusLab <kitty14904@naver.com>
2026-03-04 01:36:54 +0900

    :recycle: refactor: 이미지 업로드 예외 처리 추가 및 서비스단으로 위임

--- src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/aws/s3/S3UploadService.java
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/aws/s3/S3UploadService.java
@@ -1,5 +1,7 @@
 package com.whereyouad.WhereYouAd.infrastructure.client.aws.s3;
 
+import com.whereyouad.WhereYouAd.domains.image.exception.ImageException;
+import com.whereyouad.WhereYouAd.domains.image.exception.code.ImageErrorCode;
 import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,22 +24,39 @@ public class S3UploadService {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String uploadImage(MultipartFile file) throws IOException {
-        // 파일명 중복을 막기 위해 UUID 추가
+    public String uploadImage(MultipartFile file) {
+        // 파일 존재 여부 검증
+        if (file == null || file.isEmpty()) {
+            throw new ImageException(ImageErrorCode.EMPTY_FILE);
+        }
+
+        // 확장자 검증 (이미지만 허용)
+        validateExtension(file.getOriginalFilename());
+
         String originalFilename = file.getOriginalFilename();
         String uniqueFileName = UUID.randomUUID().toString() + "_" + originalFilename;
 
-        // 파일의 메타데이터(타입 등) 설정
         ObjectMetadata metadata = ObjectMetadata.builder()
                 .contentType(file.getContentType())
                 .build();
 
-        // S3 버킷에 업로드
         try (InputStream inputStream = file.getInputStream()) {
             s3Template.upload(bucket, uniqueFileName, inputStream, metadata);
+        } catch (IOException e) {
+            // S3 업로드 과정에서 발생하는 에러 처리
+            throw new ImageException(ImageErrorCode.IMAGE_UPLOAD_FAILED);
         }
 
-        // 업로드된 이미지의 URL 반환 (DB에 저장될 값)
-        return "http://whereyouad-s3.s3.ap-northeast-2.amazonaws.com/" + uniqueFileName;
+        // S3 객체 URL 반환
+        return String.format("https://%s.s3.ap-northeast-2.amazonaws.com/%s", bucket, uniqueFileName);
+    }
+
+    private void validateExtension(String filename) {
+        String lastDot = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+        List<String> allowedExtensions = Arrays.asList("jpg", "jpeg", "png", "webp");
+
+        if (!allowedExtensions.contains(lastDot)) {
+            throw new ImageException(ImageErrorCode.INVALID_FILE_EXTENSION);
+        }
     }
 }
