0ae4c99dd165aa1abbb34e1edeaabc995ffda849
jinnieusLab <kitty14904@naver.com>
2026-03-04 01:35:48 +0900

    :sparkles: feat: S3 이미지 업로드 응답 DTO 구조 추가

---
+++ src/main/java/com/whereyouad/WhereYouAd/domains/image/application/dto/response/ImageResponse.java
@@ -0,0 +1,8 @@
+package com.whereyouad.WhereYouAd.domains.image.application.dto.response;
+
+public class ImageResponse {
+
+    public record ImageUploadResponse (
+            String url
+    ) {}
+}
