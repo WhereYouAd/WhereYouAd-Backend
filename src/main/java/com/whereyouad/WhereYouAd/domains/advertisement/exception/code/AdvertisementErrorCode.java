a973fb8be1043bd2cf7fb23b48b831c12f9ad8db
jinnieusLab <kitty14904@naver.com>
2026-03-06 22:42:27 +0900

    :recycle: refactor: Advertisement 에러 코드 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/exception/code/AdvertisementErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/exception/code/AdvertisementErrorCode.java
@@ -13,10 +13,10 @@ public enum AdvertisementErrorCode implements BaseErrorCode {
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "AD_400_1", "날짜 입력이 잘못되었습니다."),
 
     // AdContent
-    ADCONTENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "ADCONTENT_404_1", "해당 광고를 찾을 수 없습니다"),
+    ADCONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "AD_404_1", "해당 광고를 찾을 수 없습니다"),
 
     // AdGroup
-    ADGROUP_NOT_FOUND(HttpStatus.BAD_REQUEST, "ADGROUP_404_1", "해당 광고 그룹을 찾을 수 없습니다"),
+    ADGROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "AD_404_2", "해당 광고 그룹을 찾을 수 없습니다"),
     ;
 
     private final HttpStatus httpStatus;
