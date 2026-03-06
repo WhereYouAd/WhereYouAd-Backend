a1a31cb10ebd330f6319767048dfc51c9fd64e84
오준영 <ojy654@naver.com>
2026-03-06 16:00:23 +0900

    :bug: fix: DashBoardErrorCode 오류 해결(DashBoardErrorCode, DashboardErrorCode 두개 파일 통합)

--- src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/exception/code/DashBoardErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/exception/code/DashBoardErrorCode.java
@@ -10,14 +10,13 @@ import org.springframework.http.HttpStatus;
 public enum DashBoardErrorCode implements BaseErrorCode {
 
     // 400
-    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "DASHBOARD_400_1", "날짜 입력이 잘못되었습니다."),
+    PROVIDER_NOT_VALID(HttpStatus.BAD_REQUEST, "DASH_400_1", "providerType 에 올바르지 않은 값이 입력되었습니다."),
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "DASH_400_2", "날짜 입력 방식이 잘못되었습니다."),
 
     // 403
-    ACCESS_FORBIDDEN(HttpStatus.FORBIDDEN, "DASHBOARD_403_1", "해당 조직에 대한 접근 권한이 없습니다."),
+    ACCESS_FORBIDDEN(HttpStatus.FORBIDDEN, "DASH_403_1", "해당 조직에 대한 접근 권한이 없습니다."),
     ;
-
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
-
 }
