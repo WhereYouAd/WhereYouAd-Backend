fadb3bc10ac679483655e9acbb762bcc6fbdd7d2
오준영 <ojy654@naver.com>
2026-03-06 20:48:06 +0900

    :bug: fix: Request DTO 내부 캠페인 Id 리스트에 대한 NotNull, NotEmpty 제약 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/request/ProjectRequest.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/request/ProjectRequest.java
@@ -1,6 +1,8 @@
 package com.whereyouad.WhereYouAd.domains.project.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -10,6 +12,8 @@ public class ProjectRequest {
             @NotBlank(message = "캠페인 그룹명은 필수입니다.")
             String name,
             String description,
+            @NotNull(message = "캠페인 Id 리스트는 null 일 수 없습니다.")
+            @NotEmpty(message = "최소 1개의 캠페인을 선택해야 합니다.")
             List<Long> campaignIds
     ){}
 }
