839b3a73087340dc57b942fdc25f08c282778400
jinnieusLab <kitty14904@naver.com>
2026-03-06 18:25:09 +0900

    :wrench: chore: repository 내 미사용 쿼리 삭제

--- src/main/java/com/whereyouad/WhereYouAd/domains/project/persistence/repository/ProjectRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/project/persistence/repository/ProjectRepository.java
@@ -6,11 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("select p from Project p where p.organization.id = :orgId")
     List<Project> findByOrganizationId(@Param(value = "orgId") Long orgId);
-
-    Optional<Project> findByIdandOrganizationId(Long id, Long orgId);
 }
