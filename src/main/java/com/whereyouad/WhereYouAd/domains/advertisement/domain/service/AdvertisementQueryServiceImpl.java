4a88e1df0cc624062d7c49cc08a7ca7544138c8a
jinnieusLab <kitty14904@naver.com>
2026-03-06 17:28:33 +0900

    :recycle: refactor: 쿼리 반복 호출 대신 JOIN 단일 쿼리로 조회 개선

--- src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/domain/service/AdvertisementQueryServiceImpl.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/domain/service/AdvertisementQueryServiceImpl.java
@@ -7,15 +7,6 @@ import com.whereyouad.WhereYouAd.domains.advertisement.exception.code.Advertisem
 import com.whereyouad.WhereYouAd.domains.advertisement.persistence.entity.AdContent;
 import com.whereyouad.WhereYouAd.domains.advertisement.persistence.entity.AdGroup;
 import com.whereyouad.WhereYouAd.domains.advertisement.persistence.repository.AdContentRepository;
-import com.whereyouad.WhereYouAd.domains.advertisement.persistence.repository.AdGroupRepository;
-import com.whereyouad.WhereYouAd.domains.organization.exception.code.OrgErrorCode;
-import com.whereyouad.WhereYouAd.domains.organization.exception.handler.OrgHandler;
-import com.whereyouad.WhereYouAd.domains.organization.persistence.entity.Organization;
-import com.whereyouad.WhereYouAd.domains.organization.persistence.repository.OrgRepository;
-import com.whereyouad.WhereYouAd.domains.project.exception.ProjectHandler;
-import com.whereyouad.WhereYouAd.domains.project.exception.code.ProjectErrorCode;
-import com.whereyouad.WhereYouAd.domains.project.persistence.entity.Project;
-import com.whereyouad.WhereYouAd.domains.project.persistence.repository.ProjectRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,19 +17,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class AdvertisementQueryServiceImpl implements AdvertisementQueryService {
 
-    private final OrgRepository orgRepository;
-    private final ProjectRepository projectRepository;
     private final AdContentRepository adContentRepository;
 
     @Override
     public AdvertisementResponse.AdContentInfoResponse readAdContent(Long orgId, Long projectId, Long adContentId) {
-        Organization organization = orgRepository.findById(orgId)
-                .orElseThrow(() -> new OrgHandler(OrgErrorCode.ORG_NOT_FOUND));
-
-        Project project = projectRepository.findByIdandOrganizationId(projectId, orgId)
-                .orElseThrow(() -> new ProjectHandler(ProjectErrorCode.PROJECT_NOT_FOUND));
-
-        AdContent adContent = adContentRepository.findById(adContentId)
+        AdContent adContent = adContentRepository.findByIdWithValidation(adContentId, projectId, orgId)
                 .orElseThrow(() -> new AdvertisementHandler(AdvertisementErrorCode.ADCONTENT_NOT_FOUND));
 
         AdGroup adGroup = adContent.getAdGroup();
@@ -53,13 +36,7 @@ public class AdvertisementQueryServiceImpl implements AdvertisementQueryService 
 
     @Override
     public AdvertisementResponse.AdGroupInfoResponse readAdGroup(Long orgId, Long projectId, Long adContentId) {
-        Organization organization = orgRepository.findById(orgId)
-                .orElseThrow(() -> new OrgHandler(OrgErrorCode.ORG_NOT_FOUND));
-
-        Project project = projectRepository.findByIdandOrganizationId(projectId, orgId)
-                .orElseThrow(() -> new ProjectHandler(ProjectErrorCode.PROJECT_NOT_FOUND));
-
-        AdContent adContent = adContentRepository.findById(adContentId)
+        AdContent adContent = adContentRepository.findByIdWithValidation(adContentId, projectId, orgId)
                 .orElseThrow(() -> new AdvertisementHandler(AdvertisementErrorCode.ADCONTENT_NOT_FOUND));
 
         AdGroup adGroup = adContent.getAdGroup();
