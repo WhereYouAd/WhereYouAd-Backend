131a7e67d3fb6c08491ebb456177e9d7e4ae84fb
오준영 <ojy654@naver.com>
2026-03-06 15:33:34 +0900

    :sparkles: feat: Project 생성을 위한 Request, Response DTO 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/request/ProjectRequest.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/request/ProjectRequest.java
@@ -1,4 +1,15 @@
 package com.whereyouad.WhereYouAd.domains.project.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
 public class ProjectRequest {
+
+    public record CreateRequest(
+            @NotBlank(message = "캠페인 그룹명은 필수입니다.")
+            String name,
+            String description,
+            List<Long> campaignIds
+    ){}
 }
