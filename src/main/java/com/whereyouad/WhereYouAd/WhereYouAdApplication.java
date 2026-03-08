88832b50e264669cf9ee4ebb6d629b82b77ae6aa
김민규 <alsrb0332@naver.com>
2026-03-08 15:54:26 +0900

    🔧 chore: Feign Client 설정 추가

--- src/main/java/com/whereyouad/WhereYouAd/WhereYouAdApplication.java
+++ src/main/java/com/whereyouad/WhereYouAd/WhereYouAdApplication.java
@@ -2,10 +2,13 @@ package com.whereyouad.WhereYouAd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
+@EnableAsync
 public class WhereYouAdApplication {
 
 	public static void main(String[] args) {
