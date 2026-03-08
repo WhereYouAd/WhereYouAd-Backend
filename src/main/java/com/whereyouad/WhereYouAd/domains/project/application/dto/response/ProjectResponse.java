08843efbece0d9c1b49862bd0b474580c914ea2b
오준영 <ojy654@naver.com>
2026-03-08 15:35:28 +0900

    :sparkles: feat: Project 내부 AdCampaign 조회를 위한 응답 DTO 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/response/ProjectResponse.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/project/application/dto/response/ProjectResponse.java
@@ -1,9 +1,25 @@
 package com.whereyouad.WhereYouAd.domains.project.application.dto.response;
 
+import com.whereyouad.WhereYouAd.domains.advertisement.domain.constant.Provider;
+
+import java.util.List;
+
 public class ProjectResponse {
 
     public record CreatedResponse(
             Long projectId,
             String message
     ){}
+
+    public record ProjectListResponse(
+            List<SimpleProjectResponse> projects
+    ) {}
+
+    public record SimpleProjectResponse(
+            Long projectId,
+            String name,
+            String description,
+            List<Provider> providers,
+            Double budgetUsageRate
+    ) {}
 }
