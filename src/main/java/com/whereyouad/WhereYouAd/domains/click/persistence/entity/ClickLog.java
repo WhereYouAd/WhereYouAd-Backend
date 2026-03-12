6eb41c585d68569696d543c9242a5c7e022cff81
jinnieusLab <kitty14904@naver.com>
2026-03-12 17:18:41 +0900

    :sparkles: feat: ClickLog 엔티티 코드 작성

---
+++ src/main/java/com/whereyouad/WhereYouAd/domains/click/persistence/entity/ClickLog.java
@@ -0,0 +1,37 @@
+package com.whereyouad.WhereYouAd.domains.click.persistence.entity;
+
+import com.whereyouad.WhereYouAd.domains.advertisement.persistence.entity.AdContent;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "click_log")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class ClickLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "click_log_id")
+    private Long id;
+
+    @Column(name = "ip_address", length = 45)
+    private String ipAddress;
+
+    @Column(name = "device", length = 512)
+    private String device;
+
+    @Column(name = "clicked_at", nullable = false)
+    private LocalDateTime clickedAt;
+
+    @Column(name = "is_suspect", nullable = false)
+    private Boolean isSuspect;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ad_content_id", nullable = false)
+    private AdContent adContent;
+}
