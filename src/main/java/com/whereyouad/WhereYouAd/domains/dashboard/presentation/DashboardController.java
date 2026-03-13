f53c4766acec9cdebde5ae0652bb58870454f2fc
오준영 <ojy654@naver.com>
2026-03-13 13:57:33 +0900

    :sparkles: feat: 실시간 클릭수 반환을 위한 API 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/presentation/DashboardController.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/dashboard/presentation/DashboardController.java
@@ -1,6 +1,8 @@
 package com.whereyouad.WhereYouAd.domains.dashboard.presentation;
 
+import com.whereyouad.WhereYouAd.domains.advertisement.domain.constant.Provider;
 import com.whereyouad.WhereYouAd.domains.dashboard.application.dto.response.DashboardResponse;
+import com.whereyouad.WhereYouAd.domains.dashboard.domain.service.DashboardClickService;
 import com.whereyouad.WhereYouAd.domains.dashboard.domain.service.DashboardService;
 import com.whereyouad.WhereYouAd.domains.dashboard.presentation.docs.DashboardControllerDocs;
 import com.whereyouad.WhereYouAd.global.response.DataResponse;
@@ -8,9 +10,11 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.time.LocalDate;
 
@@ -20,6 +24,7 @@ import java.time.LocalDate;
 public class DashboardController implements DashboardControllerDocs {
 
     private final DashboardService dashboardService;
+    private final DashboardClickService dashboardClickService;
 
     @GetMapping("/{orgId}/metrics")
     public ResponseEntity<DataResponse<DashboardResponse.AggregatedSummaryResponse>> getMetricsSummary(
@@ -76,5 +81,15 @@ public class DashboardController implements DashboardControllerDocs {
                 userId, orgId, adjustedStart, adjustedEnd);
 
         return ResponseEntity.ok(DataResponse.from(response));
+    }
+
+    @GetMapping(value = "/{orgId}/clicks/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> streamRealClicks(
+            @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable Long orgId,
+            @RequestParam(required = false) Provider provider
+    )
+    {
+        dashboardClickService.subscribe(userId, orgId, provider);
     }
 }
