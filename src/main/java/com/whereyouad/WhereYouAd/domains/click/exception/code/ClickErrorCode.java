2ceb1185bf424603e2f3ddc2910720d36cbcd020
김민규 <alsrb0332@naver.com>
2026-03-16 19:28:14 +0900

    ✨ feat: Click 도메인 에러 코드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/click/exception/code/ClickErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/click/exception/code/ClickErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ClickErrorCode implements BaseErrorCode {
 
+    // 403
+    CLICK_UNAUTHORIZED(HttpStatus.FORBIDDEN, "CLICK_403_1", "해당 광고에 접근할 권한이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
