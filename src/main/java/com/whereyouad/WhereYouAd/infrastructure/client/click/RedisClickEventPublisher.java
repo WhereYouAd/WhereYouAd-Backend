05b727604d4b74412709d9d057cedecc98608b6c
김민규 <alsrb0332@naver.com>
2026-03-18 11:29:36 +0900

    ✨ feat: 클릭 이벤트 redis에 LPush하여 저장하는 로직 추가

---
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/click/RedisClickEventPublisher.java
@@ -0,0 +1,36 @@
+package com.whereyouad.WhereYouAd.infrastructure.client.click;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whereyouad.WhereYouAd.domains.click.application.dto.response.ClickResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component // TODO: kafka 도입 시 어노테이션 제거
+@RequiredArgsConstructor
+// 클릭 이벤트를 redis에 LPush하는 메서드
+public class RedisClickEventPublisher implements ClickEventPublisher {
+
+    private static final String QUEUE_NAME = "click_queue";
+
+    private final StringRedisTemplate redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void publish(ClickResponse.ClickEvent event) {
+        try {
+            String jsonEvent = objectMapper.writeValueAsString(event);
+            redisTemplate.opsForList().leftPush(QUEUE_NAME, jsonEvent);
+            log.debug("redis 큐에 클릭 이벤트 저장: {}", jsonEvent);
+        }
+        catch (JsonProcessingException e) {
+            log.error("Json 객체 직렬화 실패: {}", event, e);
+        }
+        catch (Exception e) {
+            log.error("redis 큐에 클릭 이벤트 저장 실패: {}", event, e);
+        }
+    }
+}
