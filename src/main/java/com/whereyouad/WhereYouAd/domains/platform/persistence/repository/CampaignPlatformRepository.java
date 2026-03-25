2ce2a181ecf9ee68d88464478063a39701effa97
오준영 <ojy654@naver.com>
2026-03-25 15:58:40 +0900

    :sparkles: feat: 엔티티 JPA Repository 추가

---
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/CampaignPlatformRepository.java
@@ -0,0 +1,7 @@
+package com.whereyouad.WhereYouAd.domains.platform.persistence.repository;
+
+import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.CampaignPlatform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignPlatformRepository extends JpaRepository<CampaignPlatform, Long> {
+}
