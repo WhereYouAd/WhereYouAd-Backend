b8ca8c0cbd6a81e8726b01c9b67cde35efbd1b5e
오준영 <ojy654@naver.com>
2026-03-27 14:52:19 +0900

    :sparkles: feat: CampaignPlatform 엔티티와 Repository 삭제

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/CampaignPlatformRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whereyouad.WhereYouAd.domains.platform.persistence.repository;
-
-import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.CampaignPlatform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignPlatformRepository extends JpaRepository<CampaignPlatform, Long> {
-}
