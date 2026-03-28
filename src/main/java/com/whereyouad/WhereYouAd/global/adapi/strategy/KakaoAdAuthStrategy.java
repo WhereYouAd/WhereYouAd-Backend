f7c792e298d3665e4f58cfbeb4261971e03a2a80
김민규 <alsrb0332@naver.com>
2026-03-28 21:39:34 +0900

    ✨ feat: 플랫폼별 광고 API 통신 인증 헤더 제작

---
+++ src/main/java/com/whereyouad/WhereYouAd/global/adapi/strategy/KakaoAdAuthStrategy.java
@@ -0,0 +1,40 @@
+package com.whereyouad.WhereYouAd.global.adapi.strategy;
+
+import com.whereyouad.WhereYouAd.domains.advertisement.domain.constant.Provider;
+import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.PlatformConnection;
+import com.whereyouad.WhereYouAd.global.utils.AESUtil;
+import com.whereyouad.WhereYouAd.global.adapi.AdAuthStrategy;
+import com.whereyouad.WhereYouAd.global.adapi.dto.AdAuthRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoAdAuthStrategy implements AdAuthStrategy {
+
+    private final AESUtil aesUtil;
+
+    @Override
+    public Provider getProvider() {
+        return Provider.KAKAO;
+    }
+
+    @Override
+    public Map<String, String> generateHeaders(PlatformConnection connection, AdAuthRequest request) throws GeneralSecurityException {
+        Map<String, String> headers = new HashMap<>();
+
+        // 1. 암호화된 액세스 토큰 복호화
+        String accessToken = new String(aesUtil.decryptAES(connection.getAccessTokenEnc()), StandardCharsets.UTF_8);
+
+        // 헤더 생성 로직 추가
+
+
+
+        return headers;
+    }
+}
