3c322d8fbcb228af9db0fe535329cc4c64cf86c3
오준영 <ojy654@naver.com>
2026-04-05 15:25:40 +0900

    :sparkles: feat: 미사용 import 문 제거

--- src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/meta/client/MetaClient.java
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/meta/client/MetaClient.java
@@ -4,10 +4,7 @@ import com.whereyouad.WhereYouAd.infrastructure.client.meta.dto.MetaDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @FeignClient(
         name = "metaClient",
