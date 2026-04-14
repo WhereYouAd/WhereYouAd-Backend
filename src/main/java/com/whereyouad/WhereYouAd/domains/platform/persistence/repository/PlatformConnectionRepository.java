a7c367fc96b87a8ee23a6d47ad83de4194382832
김민규 <alsrb0332@naver.com>
2026-04-14 21:44:16 +0900

    ✨ feat: 연동 정보 조회 쿼리 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
@@ -10,4 +10,7 @@ public interface PlatformConnectionRepository extends JpaRepository<PlatformConn
     
     // 조직 ID와 플랫폼으로 등록된 연동 정보(Account/Connection) 목록 조회
     List<PlatformConnection> findByPlatformAccount_Organization_IdAndPlatformAccount_Provider(Long orgId, Provider provider);
+
+    // 특정 플랫폼의 모든 연동 정보 조회 (스케줄러용)
+    List<PlatformConnection> findByPlatformAccount_Provider(Provider provider);
 }
