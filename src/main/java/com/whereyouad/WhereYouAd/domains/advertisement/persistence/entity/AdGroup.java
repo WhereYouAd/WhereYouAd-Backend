d1f4f2effde694a455fdf63049070f6227ad4300
jinnieusLab <kitty14904@naver.com>
2026-05-05 15:22:15 +0900

    :wrench: chore: AdGroup 복합키 오타 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/persistence/entity/AdGroup.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/persistence/entity/AdGroup.java
@@ -15,7 +15,7 @@ import java.util.List;
         // 플랫폼 내 같은 adGroup 중복 저장 방지
         @UniqueConstraint(
                 name = "uk_ad_campaign_external_group", // 인덱스 이름
-                columnNames = {"ad_campaign_id", "exter정nal_group_id"} // 복합 키 지정
+                columnNames = {"ad_campaign_id", "external_group_id"} // 복합 키 지정
         )
 })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
