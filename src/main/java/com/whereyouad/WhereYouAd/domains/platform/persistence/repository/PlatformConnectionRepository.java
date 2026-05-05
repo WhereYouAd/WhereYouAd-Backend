9fbef1bb1a666c60d071ce944869e30bb1284682
오준영 <ojy654@naver.com>
2026-05-05 15:17:30 +0900

    :recycle: refactor: 미사용 import 문 제거

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
@@ -1,9 +1,7 @@
 package com.whereyouad.WhereYouAd.domains.platform.persistence.repository;
 
 import com.whereyouad.WhereYouAd.domains.advertisement.domain.constant.Provider;
-import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.PlatformAccount;
 import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.PlatformConnection;
-import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
