a42dd3d5a0c563762ac495003ec7860b5e24d3c5
김민규 <alsrb0332@naver.com>
2026-05-09 19:33:11 +0900

    :bug: fix: isValidRange가 JSON 응답에 포함되는 문제 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/application/dto/request/AdvertisementRequest.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/application/dto/request/AdvertisementRequest.java
@@ -1,6 +1,7 @@
 package com.whereyouad.WhereYouAd.domains.advertisement.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 
@@ -15,6 +16,7 @@ public class AdvertisementRequest {
             @JsonFormat(pattern = "yyyy-MM-dd")
             LocalDate endDate
     ) {
+        @JsonIgnore
         @AssertTrue(message = "시작 날짜는 종료 날짜보다 늦을 수 없습니다.")
         public boolean isValidRange() {
             return startDate != null && endDate != null && !startDate.isAfter(endDate);
