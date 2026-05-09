4c466ed9c91fecde85ee3a5509b18671ddeb5720
오준영 <ojy654@naver.com>
2026-05-09 20:39:06 +0900

    :recycle: refactor: 미사용 import 문 제거

--- src/main/java/com/whereyouad/WhereYouAd/domains/user/domain/service/UserService.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/domain/service/UserService.java
@@ -18,8 +18,6 @@ import com.whereyouad.WhereYouAd.domains.user.persistence.repository.UserReposit
 import com.whereyouad.WhereYouAd.global.utils.RedisUtil;
 import com.whereyouad.WhereYouAd.infrastructure.client.aws.s3.S3UploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
