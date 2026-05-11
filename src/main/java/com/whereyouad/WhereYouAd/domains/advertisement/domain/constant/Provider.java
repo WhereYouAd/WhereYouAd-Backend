5e903eda46d93abe25180285338ab108dd3810c6
오준영 <ojy654@naver.com>
2026-05-11 10:20:23 +0900

    :bug: fix: 광고 enum KAKAO 임시 추가 (Mock Data 수정 후 제거 필요)

--- src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/domain/constant/Provider.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/advertisement/domain/constant/Provider.java
@@ -1,7 +1,6 @@
 package com.whereyouad.WhereYouAd.domains.advertisement.domain.constant;
 
 public enum Provider {
-    GOOGLE, NAVER, META
-    //KAKAO
+    GOOGLE, NAVER, META, KAKAO
     // 추후 값 추가
 }
