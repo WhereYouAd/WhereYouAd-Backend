b21f288e30b258ba7ac3304d1963f238a578a641
오준영 <ojy654@naver.com>
2026-05-12 22:42:41 +0900

    :sparkles: feat: API 키 관리를 위한 조회용 Response, 수정용 Request DTO 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/application/dto/request/PlatformRequest.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/application/dto/request/PlatformRequest.java
@@ -7,4 +7,11 @@ public class PlatformRequest {
             String apiKey,       // API 키 (AES 암호화됨) → PlatformConnection.authIdentifier
             String secretKey     // Secret (AES 암호화됨) → PlatformConnection.authCredential
     ) {}
+
+    // 네이버 광고 API 수정에 사용
+    public record UpdateNaverApiRequest(
+            String customerId, // 만약 customerId 가 같다면 그대로 유지, 다르면 계정 정보 자체를 교체
+            String apiKey,
+            String secretKey
+    ) {}
 }
