88ad010948f9f9f8d50b8989357f4d4de47311ef
오준영 <ojy654@naver.com>
2026-05-12 22:49:39 +0900

    :sparkles: feat: API 연동 현황 조회, 네이버 API 연동 키 변경 API 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/presentation/PlatformController.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/presentation/PlatformController.java
@@ -10,11 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +19,7 @@ public class PlatformController implements PlatformControllerDocs {
 
     private final PlatformService platformService;
 
+    //네이버 광고 API 최초 등록 시 사용 (기존 코드 유지)
     @PostMapping("/{orgId}/accounts/naver")
     public ResponseEntity<DataResponse<PlatformResponse.PlatformAccount>> addNaverAdAccount(
             @AuthenticationPrincipal(expression = "userId") Long userId,
@@ -34,4 +31,33 @@ public class PlatformController implements PlatformControllerDocs {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(DataResponse.created(response));
     }
+
+    @GetMapping("/{orgId}/accounts")
+    public ResponseEntity<DataResponse<PlatformResponse.PlatformAccountListResponse>> getPlatformSyncInfos(
+            @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable Long orgId
+    )
+    {
+        PlatformResponse.PlatformAccountListResponse response = platformService.getPlatformSyncInfos(userId, orgId);
+
+        return ResponseEntity.ok(
+                DataResponse.from(response)
+        );
+    }
+
+    // 네이버 광고 API 수정 시 사용
+    // 네이버 제외 구글, 메타의 경우 기존 OAuth 로그인 API 를 재호출 하도록 하여 수정 진행
+    @PatchMapping("/{orgId}/accounts/naver")
+    public ResponseEntity<DataResponse<PlatformResponse.PlatformAccount>> updateNaverAdAccount(
+            @AuthenticationPrincipal(expression = "userId") Long userId,
+            @PathVariable Long orgId,
+            @Valid @RequestBody PlatformRequest.UpdateNaverApiRequest request
+    )
+    {
+        PlatformResponse.PlatformAccount response = platformService.updateNaverAdAccount(userId, orgId, request);
+        return ResponseEntity.ok(
+                DataResponse.from(response)
+        );
+    }
+
 }
