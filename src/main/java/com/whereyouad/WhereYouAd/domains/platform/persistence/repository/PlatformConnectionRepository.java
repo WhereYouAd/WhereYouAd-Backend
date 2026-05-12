00cb1b961c8683d6024c2b1ce2ca2965cfc4de86
오준영 <ojy654@naver.com>
2026-05-12 22:44:23 +0900

    :sparkles: feat: API 키 관리 로직 관련 Repository 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/persistence/repository/PlatformConnectionRepository.java
@@ -35,4 +35,7 @@ public interface PlatformConnectionRepository extends JpaRepository<PlatformConn
 
     @Query("SELECT c FROM PlatformConnection c WHERE c.user.id = :userId " + "AND c.platformAccount.id = :platformAccountId")
     Optional<PlatformConnection> findByUserIdAndPlatformAccountId(@Param("userId") Long userId, @Param("platformAccountId") Long platformAccountId);
+
+    @Query("SELECT c FROM PlatformConnection c  WHERE c.user.id = :userId AND c.platformAccount.organization.id = :orgId")
+    List<PlatformConnection> findByUserIdAndOrgId(@Param("userId") Long userId, @Param("orgId") Long orgId);
 }
