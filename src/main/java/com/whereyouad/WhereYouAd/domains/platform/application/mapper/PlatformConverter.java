14eb5ecbe2d3bc2238765345585b710c10862616
오준영 <ojy654@naver.com>
2026-05-12 22:43:21 +0900

    :sparkles: feat: DTO Converter 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/platform/application/mapper/PlatformConverter.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/platform/application/mapper/PlatformConverter.java
@@ -12,6 +12,10 @@ import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.PlatformAcc
 import com.whereyouad.WhereYouAd.domains.platform.persistence.entity.PlatformConnection;
 import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlatformConverter {
 
     // dto -> entity
@@ -55,5 +59,29 @@ public class PlatformConverter {
                 platformAccount.getProvider(),
                 platformAccount.getStatus()
         );
+    }
+
+    public static PlatformResponse.PlatformKeyResponse toPlatformKeyResponse(PlatformConnection connection) {
+        return new PlatformResponse.PlatformKeyResponse(
+                connection.getPlatformAccount().getId(),
+                connection.getPlatformAccount().getExternalAccountId(),
+                connection.getPlatformAccount().getProvider(),
+                connection.getAuthType(),
+                connection.getPlatformAccount().getStatus(),
+                connection.getTokenExpireAt() != null ? LocalDate.from(connection.getTokenExpireAt()) : null,
+                connection.getUpdatedAt() != null ? LocalDate.from(connection.getUpdatedAt()) : null
+                // TODO : 마지막 연동일자? 를 나타내는 것에 대해 논의 필요
+                // -> PlatformConnection 에 lastSyncedAt 로 필드를 추가하는게 나을지? 지금처럼 updatedAt 를 가져다 쓰는게 나을지?
+        );
+    }
+
+    public static PlatformResponse.PlatformAccountListResponse toPlatformAccountListResponse(List<PlatformConnection> connections) {
+        List<PlatformResponse.PlatformKeyResponse> responseList = new ArrayList<>();
+        for (PlatformConnection connection : connections) {
+            PlatformResponse.PlatformKeyResponse response = toPlatformKeyResponse(connection);
+            responseList.add(response);
+        }
+
+        return new PlatformResponse.PlatformAccountListResponse(responseList);
     }
 }
