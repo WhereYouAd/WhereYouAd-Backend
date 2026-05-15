cd1b4c19ee997cdc64bce915c0b28d37c5695c1c
오준영 <ojy654@naver.com>
2026-05-15 17:11:58 +0900

    :sparkles: feat: 조직 엔티티 내부 생성자 양도를 위한 비즈니스 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/entity/Organization.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/persistence/entity/Organization.java
@@ -4,6 +4,7 @@ import com.whereyouad.WhereYouAd.domains.organization.application.dto.request.Or
 import com.whereyouad.WhereYouAd.domains.organization.domain.constant.OrgStatus;
 import com.whereyouad.WhereYouAd.global.common.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
@@ -52,5 +53,9 @@ public class Organization extends BaseEntity {
 
     public void restoreDelete() {
         this.status = OrgStatus.ACTIVE;
+    }
+
+    public void changeOwner(Long newOwnerUserId) {
+        this.ownerUserId = newOwnerUserId;
     }
 }
