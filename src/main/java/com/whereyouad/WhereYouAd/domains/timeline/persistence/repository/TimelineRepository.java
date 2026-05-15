a06b8cf4aed60da65834caa3a59975ea673dbbc8
김민규 <alsrb0332@naver.com>
2026-05-15 16:22:36 +0900

    :sparkles: feat: 타임라인 조회 쿼리 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/timeline/persistence/repository/TimelineRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/timeline/persistence/repository/TimelineRepository.java
@@ -3,5 +3,8 @@ package com.whereyouad.WhereYouAd.domains.timeline.persistence.repository;
 import com.whereyouad.WhereYouAd.domains.timeline.persistence.entity.Timeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TimelineRepository extends JpaRepository<Timeline, Long> {
+    List<Timeline> findByOrganizationIdOrderByCreatedAtDesc(Long orgId);
 }
