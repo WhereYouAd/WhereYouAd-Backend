42c793d32d653237f657c418d7f8eef28606b231
오준영 <ojy654@naver.com>
2026-05-18 14:56:57 +0900

    :recycle: refactor: 조직 생성자 위임 확인 로직 순서 변경 (코드래빗)

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/domain/service/OrgServiceImpl.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/domain/service/OrgServiceImpl.java
@@ -498,17 +498,17 @@ public class OrgServiceImpl implements OrgService {
     // 조직 생성자(ownerUserId) 양도 메서드
     public void changeOwner(Long userId, Long orgId, OrgRequest.ChangeOwner request) {
 
-        // 생성자 양도를 요청한 기존 조직 생성자(userId)와, 새로운 생성자(request.newOwnerUserId()) 가 동일할 경우 오류
-        if (Objects.equals(userId, request.newOwnerUserId())) {
-            throw new OrgHandler(OrgErrorCode.ORG_OWNER_SAME_AS_BEFORE);
-        }
-
         Organization organization = orgRepository.findById(orgId)
                 .orElseThrow(() -> new OrgHandler(OrgErrorCode.ORG_NOT_FOUND));
 
         // 만약 생성자 양도를 요청한 회원이 해당 조직의 생성자 아닐 경우 오류
         if (!Objects.equals(organization.getOwnerUserId(), userId)) {
             throw new OrgHandler(OrgErrorCode.ORG_FORBIDDEN);
+        }
+
+        // 생성자 양도를 요청한 기존 조직 생성자(userId)와, 새로운 생성자(request.newOwnerUserId()) 가 동일할 경우 오류
+        if (Objects.equals(userId, request.newOwnerUserId())) {
+            throw new OrgHandler(OrgErrorCode.ORG_OWNER_SAME_AS_BEFORE);
         }
 
         // 새로운 생성자가 될 회원의 OrgMember 조회
