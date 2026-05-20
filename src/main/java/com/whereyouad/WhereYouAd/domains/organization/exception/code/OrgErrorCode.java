6b986725b7162b93b21570a124845f414a029981
오준영 <ojy654@naver.com>
2026-05-20 16:28:06 +0900

    :sparkles: feat: 조직 Hard Delete 시 PlatformConnection, PlatformAccount 존재할 경우 제약을 위한 OrgErrorCode 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/organization/exception/code/OrgErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/organization/exception/code/OrgErrorCode.java
@@ -15,6 +15,7 @@ public enum OrgErrorCode implements BaseErrorCode {
     ORG_CANNOT_ROLE_CHANGE_SELF(HttpStatus.BAD_REQUEST, "ORG_400_4", "본인의 역할은 변경할 수 없습니다."),
     ORG_LAST_ADMIN(HttpStatus.BAD_REQUEST, "ORG_400_5", "마지막 ADMIN은 강등할 수 없습니다."),
     ORG_OWNER_SAME_AS_BEFORE(HttpStatus.BAD_REQUEST, "ORG_400_6", "자기 자신으로 조직 소유자를 변경할 수 없습니다."),
+    ORG_PLATFORM_CONNECTED(HttpStatus.BAD_REQUEST, "ORG_400_7", "조직 내 연동된 광고 플랫폼 연동을 먼저 해제해주세요."),
 
     // 403
     ORG_FORBIDDEN(HttpStatus.FORBIDDEN, "ORG_403_1", "해당 요청은 조직 생성자만 요청 가능합니다."),
