eeba1187093eff746d3f441781c4d4da1fb28de3
오준영 <ojy654@naver.com>
2026-05-20 16:18:21 +0900

    :sparkles: feat: User 스케줄러에서 Hard Delete 를 위한 Repository 조회 메서드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/UserRepository.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/user/persistence/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.whereyouad.WhereYouAd.domains.user.persistence.repository;
 
+import com.whereyouad.WhereYouAd.domains.user.domain.constant.UserStatus;
 import com.whereyouad.WhereYouAd.domains.user.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -18,4 +21,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 이메일 중복 예외 처리를 위한 이메일로 조회 메서드
     boolean existsByEmail(String email);
+
+    // Soft Delete 후 일정 기간이 지난 회원 조회 (Hard Delete 대상)
+    @Query("SELECT u FROM User u WHERE u.status = :status AND u.deletedAt <= :threshold")
+    List<User> findAllByStatusAndDeletedAtBefore(@Param("status") UserStatus status,
+                                                 @Param("threshold") LocalDate threshold);
 }
