d9dfc17fc1584551e1c0b54925cc7cbf72538d91
김민규 <alsrb0332@naver.com>
2026-06-01 17:09:03 +0900

    :sparkles: feat: AppException에 bind 필드 추가

--- src/main/java/com/whereyouad/WhereYouAd/global/exception/AppException.java
+++ src/main/java/com/whereyouad/WhereYouAd/global/exception/AppException.java
@@ -2,13 +2,22 @@ package com.whereyouad.WhereYouAd.global.exception;
 
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public class AppException extends RuntimeException {
 
     private final BaseErrorCode errorCode;
+    private final Map<String, String> bind;
 
     public AppException(BaseErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.bind = null;
+    }
+
+    public AppException(BaseErrorCode errorCode, Map<String, String> bind) {
+        this.errorCode = errorCode;
+        this.bind = bind;
     }
 }
