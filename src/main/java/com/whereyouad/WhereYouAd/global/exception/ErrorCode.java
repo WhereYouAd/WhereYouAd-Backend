eb8e29c18bfcee97661bac1ddb33cf9046bdf3f8
김민규 <alsrb0332@naver.com>
2026-06-01 17:10:23 +0900

    :sparkles: feat: GlobalErrorCode 이름 체계 통일

--- src/main/java/com/whereyouad/WhereYouAd/global/exception/ErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/global/exception/ErrorCode.java
@@ -14,9 +14,12 @@ public enum ErrorCode implements BaseErrorCode{
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "COMMON-001"),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다.", "COMMON-002"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다.", "COMMON-003"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", "COMMON_400_1"),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘못되었습니다.", "COMMON_400_2"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없습니다.", "COMMON_404_1"),
 
-    //500
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.", "COMMON-004"),
+    // 500
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.", "COMMON_500_1"),
     ;
 
     private final HttpStatus httpStatus;
