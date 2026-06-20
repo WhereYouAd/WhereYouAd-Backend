b5578bf4d09f6604d95124483042127d16c69885
김민규 <alsrb0332@naver.com>
2026-06-21 00:50:29 +0900

    :bug: fix: NotificationServiceImpl implements NotificationService 누락 수정

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/service/NotificationServiceImpl.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/service/NotificationServiceImpl.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class NotificationServiceImpl {
+public class NotificationServiceImpl implements NotificationService {
 }
