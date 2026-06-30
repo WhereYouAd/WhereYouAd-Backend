2ac89779ad136609a0f54ce76fcb819828dd99ad
김민규 <alsrb0332@naver.com>
2026-06-30 18:55:30 +0900

    :sparkles: feat: 알림 관련 에러코드 추가

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/exception/code/NotificationErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/exception/code/NotificationErrorCode.java
@@ -9,6 +9,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum NotificationErrorCode implements BaseErrorCode {
 
+    // 403
+    FORBIDDEN(HttpStatus.FORBIDDEN, "NOTIFICATION_403_1", "해당 작업을 수행할 권한이 없습니다."),
+
+    // 404
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_404_1", "해당 조직의 멤버를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
