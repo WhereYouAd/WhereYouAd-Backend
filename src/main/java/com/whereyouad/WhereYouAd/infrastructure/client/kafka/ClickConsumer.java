5ff7f7e4306c185bb8702242a6e28b590f3f7cfc
jinnieusLab <kitty14904@naver.com>
2026-07-01 04:08:25 +0900

    :wrench: chore: DummyClickProducer와 ClickConsumer 내 반복 log debug 레벨로 변경

--- src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/kafka/ClickConsumer.java
+++ src/main/java/com/whereyouad/WhereYouAd/infrastructure/client/kafka/ClickConsumer.java
@@ -60,7 +60,7 @@ public class ClickConsumer {
             log.warn("orgId 누락 이벤트 수신: adId={}, mode={}", event.getAdContentId(), mode);
         }
 
-        log.info("Click Key: {}, UserAgent: {}, IP Address: {}, Count: {}",
+        log.debug("Click Key: {}, UserAgent: {}, IP Address: {}, Count: {}",
                 clickKey, event.getUserAgent(), event.getIpAddress(), currentClickCount);
     }
 
@@ -127,7 +127,7 @@ public class ClickConsumer {
 
         clickLogRepository.save(clickLog);
 
-        log.info("[DB저장] adId={}, ip={}, isSuspect={}",
+        log.debug("[DB저장] adId={}, ip={}, isSuspect={}",
                 event.getAdContentId(), event.getIpAddress(), isSuspect);
     }
 }
