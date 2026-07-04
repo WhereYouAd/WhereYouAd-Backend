2f70d353dbca14e5f695c78f9a514340c0d76b94
오준영 <ojy654@naver.com>
2026-07-04 16:01:30 +0900

    :bug: fix: 오류 코드 중복 수정 (코드래빗)

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/exception/code/NotificationErrorCode.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/exception/code/NotificationErrorCode.java
@@ -17,7 +17,7 @@ public enum NotificationErrorCode implements BaseErrorCode {
 
     // 404
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_404_1", "해당 조직의 멤버를 찾을 수 없습니다."),
-    ORG_NOTIFICATION_SETTING_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_404_1", "조직 알림 설정이 존재하지 않습니다."),
+    ORG_NOTIFICATION_SETTING_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTIFICATION_404_2", "조직 알림 설정이 존재하지 않습니다."),
 
     // 500
     NOTIFICATION_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "NOTIFICATION_500_1", "알림 발송에 실패했습니다."),
