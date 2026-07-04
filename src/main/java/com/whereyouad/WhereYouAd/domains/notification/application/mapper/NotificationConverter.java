0cb3eb2913835680a0079f78b3b4af89c6042434
오준영 <ojy654@naver.com>
2026-07-04 15:59:23 +0900

    :bug: fix: 디스코드, 슬랙 알림 제목, 본문에 대한 null 방어 추가 (코드래빗)

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/application/mapper/NotificationConverter.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/application/mapper/NotificationConverter.java
@@ -71,11 +71,13 @@ public class NotificationConverter {
     }
 
     public static DiscordMessage toDiscordMessage(String title, String message) {
-        DiscordMessage.Embed embed = new DiscordMessage.Embed(title, message, 5814783); // TODO: 디스코드는 알림 메세지 설정 가능 : 현재는 파랑색
+        DiscordMessage.Embed embed = new DiscordMessage.Embed(
+                title == null ? "" : title, message == null ? "" : message, 5814783 // TODO: 디스코드는 알림 메세지 설정 가능 : 현재는 파랑색
+        );
         return new DiscordMessage("WhereYouAd 알림", List.of(embed)); // TODO: 이름을 하드코딩 할지... 아니면 이것도 사용자에게 입력받을지..?
     }
 
     public static SlackMessage toSlackMessage(String title, String message) {
-        return new SlackMessage("*" + title + "*\n" + (message == null ? "" : message));
+        return new SlackMessage("*" + (title == null ? "" : title) + "*\n" + (message == null ? "" : message));
     }
 }
