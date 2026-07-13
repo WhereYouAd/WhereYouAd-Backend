35dee9b68b17192d09f802300b85a641099f6d5e
오준영 <ojy654@naver.com>
2026-07-13 14:40:43 +0900

    :recycle: refactor: 엔티티 내부 봇클릭 알림 & 클릭수 알림 -> 클릭 알림으로 통일

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/persistence/entity/OrgMemberNotificationSetting.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/persistence/entity/OrgMemberNotificationSetting.java
@@ -32,13 +32,9 @@ public class OrgMemberNotificationSetting extends BaseEntity {
     @Column(name = "is_email_enabled", nullable = false)
     private boolean isEmailEnabled;
 
-    @Column(name = "alert_rapid_clicks", nullable = false)
+    @Column(name = "alert_clicks", nullable = false)
     @ColumnDefault("false")
-    private boolean alertRapidClicks;
-
-    @Column(name = "alert_bot_clicks", nullable = false)
-    @ColumnDefault("false")
-    private boolean alertBotClicks;
+    private boolean alertClicks;
 
     @Column(name = "alert_report", nullable = false)
     @ColumnDefault("false")
@@ -53,9 +49,8 @@ public class OrgMemberNotificationSetting extends BaseEntity {
         if (isEmailEnabled != null) this.isEmailEnabled = isEmailEnabled;
     }
 
-    public void updateAlerts(Boolean alertRapidClicks, Boolean alertBotClicks, Boolean alertReport) {
-        if (alertBotClicks != null) this.alertBotClicks = alertBotClicks;
+    public void updateAlerts(Boolean alertClicks, Boolean alertReport) {
+        if (alertClicks != null) this.alertClicks = alertClicks;
         if (alertReport != null) this.alertReport = alertReport;
-        if (alertRapidClicks != null) this.alertRapidClicks = alertRapidClicks;
     }
 }
