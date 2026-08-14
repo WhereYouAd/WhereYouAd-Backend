8f951bc83917a8515d09b9bd9869e053a96d61ec
오준영 <ojy654@naver.com>
2026-08-14 18:16:07 +0900

    :recycle: refactor: NotificationType 값 확장

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/constant/NotificationType.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/constant/NotificationType.java
@@ -1,6 +1,8 @@
 package com.whereyouad.WhereYouAd.domains.notification.domain.constant;
 
 public enum NotificationType {
-    CLICKS,
+    BOT_CLICKS,
+    CLICKS_INCREASE,
+    CLICKS_DECREASE,
     REPORT
 }
