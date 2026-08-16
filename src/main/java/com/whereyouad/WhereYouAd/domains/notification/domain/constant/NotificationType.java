4736a3ce3a65fa28ad8f03bc6e7da487624c8935
오준영 <ojy654@naver.com>
2026-08-16 15:03:28 +0900

    :bug: fix: NotificationType enum 내부 CLICKS_DECREASE 값 제거

--- src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/constant/NotificationType.java
+++ src/main/java/com/whereyouad/WhereYouAd/domains/notification/domain/constant/NotificationType.java
@@ -3,6 +3,6 @@ package com.whereyouad.WhereYouAd.domains.notification.domain.constant;
 public enum NotificationType {
     BOT_CLICKS,
     CLICKS_INCREASE,
-    CLICKS_DECREASE,
+//    CLICKS_DECREASE,
     REPORT
 }
